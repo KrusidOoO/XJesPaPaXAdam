05b5d27aeb9e1f923ef10a1c8ec0cec87700b021
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-01-06 12:09:29 +0100

    Added basic code to the calculator
    Calculator addition

--- Calculator for GitHub/Calculator for GitHub/Program.cs
+++ Calculator for GitHub/Calculator for GitHub/Program.cs
@@ -10,6 +10,29 @@ namespace Calculator_for_GitHub
     {
         static void Main(string[] args)
         {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            ConsoleKeyInfo mitvalg;
+            Console.WriteLine("Hello there");
+            Console.WriteLine("1. Læg 2 tal sammen");
+            Console.WriteLine("2. Træk 2 tal fra hinanden");
+            Console.WriteLine("3. Gang 2 tal med hianden");
+            Console.WriteLine("4. Divider 2 tal med hinanden");
+            mitvalg = Console.ReadKey(true);
+            if (mitvalg.KeyChar == '1')
+            {
+                Console.WriteLine("You have now chosen to add two digits together");
+                Console.WriteLine();
+                Console.WriteLine("Please enter your first digit");
+                string firstNumber1 = Console.ReadLine();
+                firstNumber = Convert.ToInt32(firstNumber1);
+                Console.WriteLine(firstNumber);
+                Console.WriteLine("Please enter your second digit");
+                string secondNumber2 = Console.ReadLine();
+                secondNumber = Convert.ToInt32(secondNumber2);
+                Console.WriteLine(secondNumber);
+            }
+            Console.ReadKey();
         }
     }
 }
