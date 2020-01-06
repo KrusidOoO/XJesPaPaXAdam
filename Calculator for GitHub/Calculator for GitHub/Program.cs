e19da5b30c5825e247cee69172c24809e753be51
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-01-06 12:41:08 +0100

    Addition of the calculator completed
    A part of the calculator has been completed, so now you can add two numbers together

--- Calculator for GitHub/Calculator for GitHub/Program.cs
+++ Calculator for GitHub/Calculator for GitHub/Program.cs
@@ -14,25 +14,35 @@ namespace Calculator_for_GitHub
             int secondNumber = 0;
             ConsoleKeyInfo mitvalg;
             Console.WriteLine("Hello there");
-            Console.WriteLine("1. Læg 2 tal sammen");
-            Console.WriteLine("2. Træk 2 tal fra hinanden");
-            Console.WriteLine("3. Gang 2 tal med hianden");
-            Console.WriteLine("4. Divider 2 tal med hinanden");
+            Console.WriteLine();
+            Console.WriteLine("1. Add two numbers together");
+            Console.WriteLine("2. Subtract two numbers");
+            Console.WriteLine("3. Multiply two numbers");
+            Console.WriteLine("4. Divide two numbers");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Please enter the number of the action you wish to perform");
             mitvalg = Console.ReadKey(true);
             if (mitvalg.KeyChar == '1')
             {
+                Console.Clear();
                 Console.WriteLine("You have now chosen to add two digits together");
                 Console.WriteLine();
                 Console.WriteLine("Please enter your first digit");
                 string firstNumber1 = Console.ReadLine();
                 firstNumber = Convert.ToInt32(firstNumber1);
-                Console.WriteLine(firstNumber);
+                Console.WriteLine();
                 Console.WriteLine("Please enter your second digit");
                 string secondNumber2 = Console.ReadLine();
                 secondNumber = Convert.ToInt32(secondNumber2);
-                Console.WriteLine(secondNumber);
+                Console.WriteLine();
+                int sum = firstNumber + secondNumber;
+                Console.WriteLine("The result is " + sum.ToString());
+                Console.ReadKey();
             }
-            Console.ReadKey();
+            if(mitvalg.KeyChar=='2')
+            {
+                Console.Clear();
+            }
         }
     }
 }
