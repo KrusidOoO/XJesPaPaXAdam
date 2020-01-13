712155f96389ed506ef6763b1f00bd435b10832f
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-01-13 09:36:29 +0100

    Changed the hello message
    Changed the hello message in the calculator program

--- Calculator for GitHub/Calculator for GitHub/Program.cs
+++ Calculator for GitHub/Calculator for GitHub/Program.cs
@@ -13,7 +13,8 @@ namespace Calculator_for_GitHub
             int firstNumber = 0;
             int secondNumber = 0;
             ConsoleKeyInfo mitvalg;
-            Console.WriteLine("Hello there");
+            //Changed the hello message
+            Console.WriteLine("Hello there, from KrusidOoO");
             Console.WriteLine();
             Console.WriteLine("1. Add two numbers together");
             Console.WriteLine("2. Subtract two numbers");
