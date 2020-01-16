61f5b678ff05acff42c26583fe125b55b322bc41
Nobiisme-JesPaPa <59558986+Nobiisme-JesPaPa@users.noreply.github.com>
2020-01-16 13:55:50 +0100

    OOP foreach sange

---
+++ Foreach sange/Foreach sange/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Foreach_sange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sange = { "Old time road", "Paris", "SOS", "So Am I" };
+
+
+
+            Console.WriteLine("Her er mine ynglingssange");
+
+
+
+            foreach (string i in Sange)
+
+            {
+
+                Console.WriteLine(i);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
