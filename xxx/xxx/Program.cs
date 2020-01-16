d23c58aacd8b63343ef56446c0b9819e284456a1
Nobiisme-JesPaPa <59558986+Nobiisme-JesPaPa@users.noreply.github.com>
2020-01-16 13:27:43 +0100

    OOP opg 1

---
+++ xxx/xxx/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace xxx
+{
+    /*
+
+ * C# Program to Accept the Height of a Person & Categorize as
+
+ * Tall, Dwarf or Average
+
+ */
+
+    class program
+
+    {
+
+        public static void Main()
+
+        {
+
+            float height;
+
+            Console.WriteLine("Enter  the Height (in centimeters) \n");
+
+            height = int.Parse(Console.ReadLine());
+
+            if (height < 150.0)
+
+                Console.WriteLine("Dwarf \n");
+
+            else if ((height > 150.0) && (height <= 165.0))
+
+                Console.WriteLine(" Average Height \n");
+
+            else if ((height >= 165.0) && (height <= 195.0))
+
+                Console.WriteLine("Prett Tall \n");
+
+            else
+
+                Console.WriteLine("Ginormus height \n");
+
+            Console.ReadKey();
+        }
+
+    }
+}
