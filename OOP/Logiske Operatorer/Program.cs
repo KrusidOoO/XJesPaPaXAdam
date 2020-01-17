8d925a79d7605937d2d1e02271273cf057b9a64a
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-01-17 13:12:29 +0100

    Added Logiske Operatorer
    Added and completed the assignment "Logiske Operatorer 1"

---
+++ OOP/Logiske Operatorer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Logiske_Operatorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; string password;
+            Console.WriteLine("Input your username here");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Input your password here");
+            password = Console.ReadLine();
+            Console.WriteLine();
+            // evaluating both expresson and returns true if all are true.
+            if (name == "Steven" && password == "Steven123")
+            {
+                Console.Clear();
+                Console.WriteLine("======================");
+                Console.WriteLine("===================");
+                Console.WriteLine("================");
+                Console.WriteLine("Login Successful");
+                Console.WriteLine("================");
+                Console.WriteLine("===================");
+                Console.WriteLine("======================");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("======================");
+                Console.WriteLine("================");
+                Console.WriteLine("Unauthorised access");
+                Console.WriteLine("================");
+                Console.WriteLine("======================");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
