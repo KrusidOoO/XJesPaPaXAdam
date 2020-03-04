50268073b8301fbfcf2559eca016f0f543104817
Nobiisme-JesPaPa <59558986+Nobiisme-JesPaPa@users.noreply.github.com>
2020-03-04 11:07:55 +0100

    ja
    Os ja

---
+++ Database/SQL projekt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace SQL_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (SqlConnection connection = new SqlConnection("Server=.;Database=gotta learn sql and it has to be fast"))
+            {
+                connection.Open();
+                Console.WriteLine("Connection established.");
+
+            }
+        }
+
+        static void MainMenu(SqlConnection connection)
+        {
+            while(true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Make A New List");
+                Console.WriteLine("Create A New Item");
+                Console.WriteLine("Change Item Values");
+                Console.WriteLine("Delete Items:");
+                ConsoleKeyInfo choice = Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+    }
+}
